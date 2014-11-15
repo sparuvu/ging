edbb3c1872d87a4f8b6c806b42c46a20d53262ac
Sundeep Paruvu <sparuvu@gmail.com>
2014-11-15 12:10:48 -0600

    updated the grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -341,7 +341,8 @@ module.exports = function (grunt) {
             '*.html',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
-            'fonts/{,*/}*.*'
+            'fonts/{,*/}*.*',
+            'bower_components/bootstrap-sass-official/vendor/assets/fonts/bootstrap/*.*'
           ]
         }, {
           expand: true,
