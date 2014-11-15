8d7097c69249da501c25b04b353ca06924a7c589
Sundeep Paruvu <sparuvu@gmail.com>
2014-11-15 10:27:48 -0600

    updated the partial path

--- app/scripts/directives/pagination.js
+++ app/scripts/directives/pagination.js
@@ -2,6 +2,6 @@ var gingApp = gingApp || angular.module('gingApp');
 
 gingApp.directive('resultsPagination', function(){
     return {
-      templateUrl: 'views/_pagination.html'
+      templateUrl: 'views/pagination.html'
     };
 });
